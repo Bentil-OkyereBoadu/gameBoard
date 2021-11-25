fa266911f35ba05ca51da4364822abc06c80608a
Bentil-OkyereBoadu <bentil.boadu@amalitech.org>
2021-11-25 17:55:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 var main = function() {
+    //click more button to show siblings
     $('.more-btn').on('click', event => {
-      $(event.currentTarget).next().toggle();
+      $(event.currentTarget).siblings('.more-menu').toggle();
     });
 
+    //showing the share menu when share is clicked
     $('.share').on('click', event => {
         $(event.currentTarget).next('.share-menu').toggle();
     })
 
+    //turning notification bell icons to yellow
     $('.notification').on('click', event => {
-        $(event.currentTarget).css({
-            color: 'green'
-        })
-    })
+        $(event.currentTarget).addClass('.active');
+    });
+
+    $()
+
   };
   
   $(document).ready(main);
