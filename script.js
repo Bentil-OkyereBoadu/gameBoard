6305d09141a88188d44b2e08b2a77a629a7adf95
Bentil-OkyereBoadu <bentil.boadu@amalitech.org>
2021-11-25 16:36:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var main = function() {
     $('.more-btn').on('click', event => {
-      
-    })
+      $(event.currentTarget).toggle('.more-menu');
+    });
+    
   };
   
   $(document).ready(main);
